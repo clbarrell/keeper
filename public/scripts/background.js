e5a70c95a35da2a1204b0706c1ca8e1d8724c62a
Chris Barrell <chris@cultureamp.com>
2020-07-25 13:35:10 +1000

    Dont' get input on weekends

--- public/scripts/background.js
+++ public/scripts/background.js
@@ -13,45 +13,63 @@ chrome.tabs.onActivated.addListener((t) => {
     } else {
       // ONLY DO IF A SAFE WEBSITE
       // add to list of timestamps of tabChanges
-      chrome.storage.local.get(["tabChanges", "changeThreshold", "lastAlert"], function (result) {
-        const tabChanges = result.tabChanges || [];
-        const changeThreshold = result.changeThreshold || 5;
-        const lastAlert = result.lastAlert || 0;
-        tabChanges.push(Date.now());
-        // save it again
-        chrome.storage.local.set({ tabChanges: tabChanges });
+      chrome.storage.local.get(
+        ["tabChanges", "changeThreshold", "lastAlert"],
+        function (result) {
+          const tabChanges = result.tabChanges || [];
+          const changeThreshold = result.changeThreshold || 5;
+          const lastAlert = result.lastAlert || 0;
+          tabChanges.push(Date.now());
+          // save it again
+          chrome.storage.local.set({ tabChanges: tabChanges });
 
-        // check if there's been more than threshold tab changes
-        const fiveMins = 1000 * 60 * 5;
-        const limit = Date.now() - fiveMins;
-        const currentHr = new Date().getHours();
-        const numOfRecentTabChanges = tabChanges.filter((tc) => tc > limit).length;
-        if (
-          numOfRecentTabChanges > changeThreshold &&
-          lastAlert < limit - fiveMins &&
-          currentHr > 8 &&
-          currentHr < 20
-        ) {
-          console.log(`Getting mood input \nnow:\t${new Date()}, \nlast:\t${new Date(lastAlert)}`);
-          chrome.storage.local.set({ lastAlert: Date.now(), lastActiveTab: currentTabId });
-          chrome.tabs.create({ url: chrome.runtime.getURL("scripts/moodModal.html") });
+          // check if there's been more than threshold tab changes
+          const fiveMins = 1000 * 60 * 5;
+          const limit = Date.now() - fiveMins;
+          const date = new Date();
+          const currentHr = date.getHours();
+          const numOfRecentTabChanges = tabChanges.filter((tc) => tc > limit)
+            .length;
+          if (
+            numOfRecentTabChanges > changeThreshold &&
+            lastAlert < limit - fiveMins &&
+            currentHr > 8 &&
+            currentHr < 19 &&
+            ![0, 6].includes(date.getDay())
+          ) {
+            console.log(
+              `Getting mood input \nnow:\t${new Date()}, \nlast:\t${new Date(
+                lastAlert
+              )}`
+            );
+            chrome.storage.local.set({
+              lastAlert: Date.now(),
+              lastActiveTab: currentTabId,
+            });
+            chrome.tabs.create({
+              url: chrome.runtime.getURL("scripts/moodModal.html"),
+            });
+          }
         }
-      });
+      );
     }
   });
 });
 
 const debug = () => {
-  chrome.storage.local.get(["tabChanges", "tabActivity", "moodRatings", "lastAlert"], function (result) {
-    console.log("TAB CHANGES");
-    console.log(result.tabChanges);
-    console.log("TAB ACTIVITY");
-    console.log(result.tabActivity);
-    console.log("MOOD RATINGS");
-    console.log(result.moodRatings);
-    console.log("Last alert");
-    console.log(new Date(result.lastAlert));
-  });
+  chrome.storage.local.get(
+    ["tabChanges", "tabActivity", "moodRatings", "lastAlert"],
+    function (result) {
+      console.log("TAB CHANGES");
+      console.log(result.tabChanges);
+      console.log("TAB ACTIVITY");
+      console.log(result.tabActivity);
+      console.log("MOOD RATINGS");
+      console.log(result.moodRatings);
+      console.log("Last alert");
+      console.log(new Date(result.lastAlert));
+    }
+  );
 };
 
 const lastActiveTab = () => {
